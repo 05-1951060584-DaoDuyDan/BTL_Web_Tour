3f989987f0bed2bb9b24310aba2eac34a3412c23
PhamLeDung <dungp2901@gmail.com>
2021-12-27 00:13:21 +0700

    update Dung 27/12/2021

---
+++ business.php
@@ -0,0 +1,40 @@
+<?php
+  require('partials-front/header-business.php')
+?>
+  <link rel="stylesheet" href="css/style2.css">
+    
+    <!-- <header>AAAA</header> -->
+    <div class="tao1">
+      <main class="container-fuild text-center align-text-stretch" >
+        <h1>Tạo tài khoản kinh doanh</h1>
+        <h5>
+          Bạn chưa sở hữu tài khoản kinh doanh, khởi tạo ngay để bắt đầu quá trình
+          hoạt động kinh doanh trên Hahalolo cho doanh nghiệp của bạn. Với một tài
+          khoản kinh doanh trên Hahalolo, bạn có thể điều hành, quản lý và phát
+          triển doanh nghiệp một cách toàn diện.
+        </h5>
+        <h6 class="h6a text-success">Tất cả trong một và hoàn toàn miễn phí.</h6>
+        <button class="btn btn-success">
+          <a href="" class=" text-decoration-none text-white">TẠO TÀI KHOẢN KINH DOANH</a>
+        </button>
+      </main>
+     
+    </div>
+    <section class="tao3 d-flex justify-content-center p-2 bg-info fixed-bottom ">
+      <hr />
+      <p class="taocon">Hahalolo &copy; 2017</p>
+      <a href="http://localhost/baitaplon/business.php" class="taocon text-decoration-none"> Giới thiệu</a>
+      <a href="https://tuyendung.hahalolo.com/" class="taocon text-decoration-none"> Tuyển dụng</a>
+      <a href="https://help.hahalolo.com/hc/vi/articles/360036293871" class="taocon text-decoration-none"> Quyền riêng tư</a>
+      <a href="https://help.hahalolo.com/hc/vi/articles/360035943132" class="taocon text-decoration-none"> Cookies</a>
+      <a href="https://help.hahalolo.com/hc/vi/articles/360036291411" class="taocon text-decoration-none"> Điều khoản</a>
+      <a href="https://help.hahalolo.com/hc/vi/articles/360035841292" class="taocon text-decoration-none"> Tiêu chuẩn cộng đồng</a>
+      <a href="https://help.hahalolo.com/hc/vi" class="taocon text-decoration-none"> Hỗ trợ</a>
+    </section>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
