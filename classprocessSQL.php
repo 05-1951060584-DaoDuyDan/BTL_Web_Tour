d439972369201088ad50717313dc694cf5a4f492
05-1951060584-DaoDuyDan <daodan2612@gmail.com>
2022-01-10 14:01:11 +0700

    xu ly class connection

--- classprocessSQL.php
+++ classprocessSQL.php
@@ -1,5 +1,17 @@
 <?php
+const DB_HOST = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'ho_mvc';
 class Process{
+    public function connectDb() {
+        $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        if (!$connection) {
+            die("Không thể kết nối. Lỗi: " .mysqli_connect_error());
+        }
+
+        return $connection;
+    }
     
 }
 ?>
