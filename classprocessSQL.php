5383d8f669695b0b0baa5d640b16bb68ea5e47b8
05-1951060584-DaoDuyDan <daodan2612@gmail.com>
2022-01-10 14:06:06 +0700

    chinh sua class connection SQL

--- classprocessSQL.php
+++ classprocessSQL.php
@@ -2,7 +2,7 @@
 const DB_HOST = 'localhost';
 const DB_USERNAME = 'root';
 const DB_PASSWORD = '';
-const DB_NAME = 'ho_mvc';
+const DB_NAME = 'db_tournew';
 class Process{
     public function connectDb() {
         $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -12,6 +12,21 @@ class Process{
 
         return $connection;
     }
-    
+    public function closeDb($connection = null) {
+        mysqli_close($connection);
+    }
+    public function getUser($idUser){
+        $connection = $this->connectDb();
+        $querySelect = "SELECT * FROM tb_user WHERE id_user={$idUser}";
+        $results = mysqli_query($connection, $querySelect);
+        $nvArr = [];
+        if (mysqli_num_rows($results) > 0) {
+            $bds = mysqli_fetch_all($results, MYSQLI_ASSOC);
+            $userArr = $bds[0];
+        }
+        $this->closeDb($connection);
+
+        return $userArr;
+    }
 }
 ?>
