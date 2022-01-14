c1a43015bde4baba19997e496a930b86bd437c81
05-1951060584-DaoDuyDan <daodan2612@gmail.com>
2022-01-15 03:16:36 +0700

    tiep tuc xu ly backend cart

--- cart.php
+++ cart.php
@@ -15,3 +15,20 @@ include('.//partials-front/header.php');
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="style/style.css">
   </head>
+
+  <body>
+  <header class="container" style="margin-top: 66px;">
+    <div class="row">
+      <h1>Giỏ hàng</h1>
+      <p>Tour(1)</p>
+      <div class="col-md border border-1 p-3 cart-content bg-white">
+        <div class="col-md-2 mt-3">
+          <img src="images/Tour/DaNang_BaNa_NguHanhSon_HoiAn.png" class="img-fluid" alt=""style="max-width: 150px; max-height: 100%">
+        </div>
+        <div class="col-md-3 me-5" style="text-align: justify;">
+          <h4>003: Đà Nẵng – Sơn Trà – Hội An – Rừng Dừa 7 Mẫu – Bà Nà (4N3Đ)</h4>
+          <p>Lịch trình <br>
+            Thành phố Đà Nẵng, Vietnam - Thành phố Đà Nẵng, Vietnam<br>
+            Số khách: 1 người lớn<br>
+            Loại tour: Trong nước</p>
+        </div>
