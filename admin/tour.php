f961fa1e15d5cf4063301a3d74ededa5fe997109
PhamLeDung <dungp2901@gmail.com>
2022-01-14 23:40:31 +0700

    add admin user

--- admin/tour.php
+++ admin/tour.php
@@ -1,9 +1,5 @@
 <?php include('partials-front/header.php')
-    // session_start(); //Dịch vụ bảo vệ
-    // if(!isset($_SESSION['loginOK']) || $_SESSION['loginOK'] != 'admin'){
-    //     header("Location:../login.php");
-    // }
-    //     include '../config.php';
+    
 ?>
 
 <body class="bg-cuatoi">
