26ecf3a12a31949d3f0a0133f49829407334bf20
unknown <leducthang462001@gmail.com>
2022-01-16 01:04:49 +0700

    update

--- tourmanager.php
+++ tourmanager.php
@@ -5,6 +5,7 @@
     // }
     //     include '../config.php';
 ?>
+
 <body >
     <div class="container ">
         <a href="add-room.php" class="btn btn-success m-2">Thêm Tour</a>
@@ -17,3 +18,13 @@
                         <th scope="col">Loại hình kinh doanh</th>
                         <th scope="col">Email</th>
                         <th scope="col">Số điện thoại</th>
+                        <th scope="col">Ảnh người điều hành</th>
+                        <th scope="col">Trạng thái</th>
+                        <th scope="col">Khóa</th>
+                        <th scope="col">Email liên kết</th>
+                        <th scope="col">Liên kết vào</th>
+                        <th scope="col">Trang</th>
+                        <th scope="col">Tên người dùng</th>
+                        <th scope="col">Khóa</th>
+                        <th scope="col">Mở</th>
+                    </tr>
