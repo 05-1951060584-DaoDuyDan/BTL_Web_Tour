05167d086481440f77a5c5fdb1a0daafe4ce140d
unknown <leducthang462001@gmail.com>
2022-01-16 01:02:57 +0700

    update

--- tourmanager.php
+++ tourmanager.php
@@ -1,98 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>admin</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-</head>
-
-<body>
-    <div class="container">
-        <div class="col-md-12">
-            <table class="table">
+<?php include('partials-front/header.php')
+    // session_start(); //Dịch vụ bảo vệ
+    // if(!isset($_SESSION['loginOK']) || $_SESSION['loginOK'] != 'admin'){
+    //     header("Location:../login.php");
+    // }
+    //     include '../config.php';
+?>
+<body >
+    <div class="container ">
+        <a href="add-room.php" class="btn btn-success m-2">Thêm Tour</a>
+        <div class="row">
+            <table class="table table-striped">
                 <thead>
                     <tr>
                         <th scope="col">STT</th>
-                        <th scope="col">Ten phòng</th>
-                        <th scope="col">Giá phòng/1 ngày</th>
-                        <th scope="col">Phù hợp cho</th>
-                        <th scope="col">Loại phòng</th>
-                        <th scope="col">Khách sạn</th>
-                        <th scope="col">Trạng thái</th>
-                        <th scope="col">Khóa tour</th>
-                        <th scope="col">Mở tour</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Phòng số 1 khu A</td>
-                        <td>350000</td>
-                        <td>1</td>
-                        <td>Phòng 3 giường ngủ</td>
-                        <td>Essenza hotel</td>
-                        <td>Không hoạt động</td>
-                        <td>
-                            <a href="#" class="m-l-42">
-                                <i class="bi bi-lock-fill" style="color:blue"></i>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="m-l-42">
-                                <i class="bi bi-unlock-fill" style="color:red"></i>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Phòng số 1 khu D</td>
-                        <td>370000</td>
-                        <td>2</td>
-                        <td>Phòng 4 giường ngủ</td>
-                        <td>Hoàng gia hotel</td>
-                        <td>Không hoạt động</td>
-                        <td>
-                            <a href="#" class="m-l-42">
-                                <i class="bi bi-lock-fill" style="color:blue"></i>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="m-l-42">
-                                <i class="bi bi-unlock-fill" style="color:red"></i>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Phòng vip</td>
-                        <td>4700000</td>
-                        <td>2</td>
-                        <td>Phòng 4 giường ngủ,có ban công</td>
-                        <td>Á đông hotel</td>
-                        <td>Không hoạt động</td>
-                        <td>
-                            <a href="#" class="m-l-42">
-                                <i class="bi bi-lock-fill" style="color:blue"></i>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="m-l-42">
-                                <i class="bi bi-unlock-fill" style="color:red"></i>
-                            </a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+                        <th scope="col">Tên người điều hành</th>
+                        <th scope="col">Loại hình kinh doanh</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Số điện thoại</th>
