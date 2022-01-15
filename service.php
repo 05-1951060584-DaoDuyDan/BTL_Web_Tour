f6ff9a8247b38f0ba94e0a629defa50ae0272cc8
unknown <leducthang462001@gmail.com>
2022-01-16 01:23:08 +0700

    update

--- service.php
+++ service.php
@@ -28,3 +28,13 @@
                 $result = mysqli_query($conn, $sql);
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <tr>
+                            <td><?php echo $row['id_tourservice']; ?></td>
+                            <td><?php echo $row['nameservice']; ?></td>
+                            <td><?php echo $row['priceservice']; ?></td>
+                            <td><a data-bs-toggle="modal" data-bs-target="#editService" class="edit" id_tourservice="<?php echo $row['id_tourservice']; ?>"><i class="fas fa-user-edit" style="color:blue"></i></a></td>
+                            <td><a data-bs-toggle="modal" data-bs-target="#deleteService" class="delete" id_tourservice="<?php echo $row['id_tourservice']; ?>" href="./deleteService.php?id=<?php echo $row['id_tourservice']; ?>"><i class="fas fa-user-times" style="color:red"></i></a></td>
+                        </tr>
+                <?php
+                    }
