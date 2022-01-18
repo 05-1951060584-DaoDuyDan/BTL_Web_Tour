5d5a4657f352c9db3eb8d8311d2855f6dfb40e20
05-1951060584-DaoDuyDan <daodan2612@gmail.com>
2022-01-19 02:13:28 +0700

    update 19/1/2022 2

--- process-updateInforUser copy.php
+++ process-updateInforUser copy.php
@@ -7,16 +7,3 @@ if (!isset($_SESSION['LoginOK'])) {
         $nameUser = $_POST['nameuser'];
         $surnameUser = $_POST['surnameuser'];
         $data = file_get_contents($_FILES['imgUserUpdate']['tmp_name']);
-        $email = $_POST['emailUpdate'];
-        $dbh = new PDO("mysql:host=localhost;dbname=db_tournew", "root", "");
-        $stmt = $dbh->prepare("UPDATE `tb_user` SET `nameuser` = ?, `surnameuser` = ?, `imageuser` = ? WHERE `email` = ?");
-        $stmt->bindParam(1, $nameUser);
-        $stmt->bindParam(2, $surnameUser);
-        $stmt->bindParam(3, $data);
-        $stmt->bindParam(4, $email);
-        if($stmt->execute()){
-            header("location: userinformation.php");
-        }
-    }
-}
-?>
