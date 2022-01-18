f0aff0b5c424e350b575013344dd8f6441283966
05-1951060584-DaoDuyDan <daodan2612@gmail.com>
2022-01-19 02:17:54 +0700

    update 19/1/2022 5

--- process-informationLock.php
+++ process-informationLock.php
@@ -11,3 +11,45 @@ if(isset($_SESSION['LoginOK'])){
                             <h4 class="text-warning text-center">Tài khoản của bạn đã bị khóa vui lòng liên hệ Admin để được hỗ trợ!</h4>
                             <br>
                             <h5 class="text-warning text-center">0366.889.368 & Daodan2612@gmail.com</h5>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    <?php
+    }else if(substr($_SESSION['LoginOK'], 0, 1) == '2'){
+    ?>
+        <main style="margin-top: 70px; margin-bottom:70px">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 ms-auto me-auto">
+                        <div class="bg-dark shadow-sm rounded p-2">
+                            <h4 class="text-warning text-center">  Tài khoản kinh doanh của bạn đã bị khóa vui lòng liên hệ Admin để được hỗ trợ!</h4>
+                            <br>
+                            <h5 class="text-warning text-center">0366.889.368 & Daodan2612@gmail.com</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    <?php
+    }
+}else if(!isset($_SESSION['LoginOK'])){
+    ?>
+        <main style="margin-top: 70px; margin-bottom:70px">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 ms-auto me-auto">
+                        <div class="bg-dark shadow-sm rounded p-2">
+                            <h4 class="text-warning text-center">  Tài khoản người dùng của bạn đã bị khóa vui lòng liên hệ Admin để được hỗ trợ!</h4>
+                            <br>
+                            <h5 class="text-warning text-center">0366.889.368 & Daodan2612@gmail.com</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    <?php
+}
+require "partials-front/footer.php";
+?>
